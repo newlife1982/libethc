fcb804a8fc43c24f857cfd576aae6516f8e07005
newlife1982 <newlife1982@protonmail.com>
2025-11-29 20:44:33 +0800

    del log

--- src/abi.c
+++ src/abi.c
@@ -749,8 +749,6 @@ int eth_abi_bytes(struct eth_abi *abi, uint8_t **bytes, size_t *len) {
     ethc_abi_buf_pr64(dyoffset, cframebuf, cframebuf->offset);
     cframebuf->offset += ETH_ABI_WORD_SIZE;
 
-    printf("dyoffset=%d cframebuf->offset=%d\n", (int)dyoffset, (int)cframebuf->offset);
-
     /* read the length */
     ethc_abi_buf_pr64(blen, cframebuf, dyoffset);
 
@@ -812,8 +810,6 @@ int eth_abi_bytes_ext(struct eth_abi *abi, uint8_t **bytes, size_t *len, size_t 
     /* read the offset */
     ethc_abi_buf_pr64(dyoffset, cframebuf, cframebuf->offset);
     cframebuf->offset += ETH_ABI_WORD_SIZE;
-
-    printf("dyoffset=%d cframebuf->offset=%d\n", (int)dyoffset, (int)cframebuf->offset);
 
     /* read the length */
     ethc_abi_buf_pr64(blen, cframebuf, dyoffset);

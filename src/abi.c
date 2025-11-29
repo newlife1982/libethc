b877044731ad48cc27ac73d6dd1b0a9f3e188f44
newlife1982 <newlife1982@protonmail.com>
2025-11-29 19:52:37 +0800

    update

--- src/abi.c
+++ src/abi.c
@@ -749,7 +749,7 @@ int eth_abi_bytes(struct eth_abi *abi, uint8_t **bytes, size_t *len) {
     ethc_abi_buf_pr64(dyoffset, cframebuf, cframebuf->offset);
     cframebuf->offset += ETH_ABI_WORD_SIZE;
 
-    printf("dyoffset=%" PRIu64 "\n cframebuf->offset=%" PRIu64 "\n", dyoffset, cframebuf->offset);
+    printf("dyoffset=%lld cframebuf->offset=%lld\n", dyoffset, cframebuf->offset);
 
     /* read the length */
     ethc_abi_buf_pr64(blen, cframebuf, dyoffset);

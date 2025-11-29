984b040b168bdc8cc5db8ea50e9c847e370882c3
newlife1982 <newlife1982@protonmail.com>
2025-11-29 19:45:34 +0800

    update

--- src/abi.c
+++ src/abi.c
@@ -748,6 +748,8 @@ int eth_abi_bytes(struct eth_abi *abi, uint8_t **bytes, size_t *len) {
     ethc_abi_buf_pr64(dyoffset, cframebuf, cframebuf->offset);
     cframebuf->offset += ETH_ABI_WORD_SIZE;
 
+    printf("dyoffset=%d cframebuf->offset=%d\n", dyoffset, cframebuf->offset);
+
     /* read the length */
     ethc_abi_buf_pr64(blen, cframebuf, dyoffset);
 
